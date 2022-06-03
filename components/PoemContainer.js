df554644482db6bf0e3e624c0427a5ba5e636af0
Warren C. Longmire <warrenlongmire@gmail.com>
2022-06-03 17:10:15 -0400

    adds in title and text animations.

--- components/PoemContainer.js
+++ components/PoemContainer.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import Image from 'next/image'
 import Slider from 'react-slick';
+import { Random, Wave } from 'react-animated-text';
 import moment from 'moment';
 
 export default class SlickGoTo extends React.Component {
     state = {
-      currentStage: 0,
-      version:2, 
-      poems:[
+        stageIndex: 0,
+        sliderValue: 0,
+        version:2,
+        isSelecting:false,
+        stages:[
           {
             id: 1,
+            title:"Wellness Plan/Mood Episode",
             date: moment().subtract(5, 'years')
           },
           {
             id: 2,
+            title:"Wellness/Episode",
             date: moment().subtract(3, 'years')
           },
           {
             id: 3,
+            title:"BOX",
             date: moment().subtract(1, 'months')
           }
       ]
@@ -30,31 +36,49 @@ export default class SlickGoTo extends React.Component {
             speed: 1000,
             slidesToShow: 1,
             slidesToScroll: 1,
-            beforeChange: (current, next) => this.setState({ currentStage: next })
+            beforeChange: (current, next) => this.setState({ stageIndex: next })
         };
     
-        const { version, poems, currentStage } = this.state;
-        const currentPoem = poems[currentStage];
-        const versionText = (currentStage == 0)?"Original":`Version ${currentStage}`;
+        const { version, stages, stageIndex } = this.state;
+        const currentStage = stages[stageIndex];
+        const versionText = (stageIndex == 0)?"Original":`Version ${stageIndex}`;
         
         return <div>
-            
+            <h2>
+                <Random
+                    text={currentStage.title}
+                    iterations="1"
+                    effect="verticalFadeIn"
+                    effectDirection="up"
+                    paused={this.state.isSelecting}
+                    effectChange={0.1}/>
+            </h2>
             <Slider ref={slider => (this.slider = slider)} {...settings}>
                 {
-                    poems.map((poem, i) => (<div>
+                    stages.map((poem, i) => (<div>
                         <Image src={`/poems/poem.1.${i+1}.v${version}.png`} width="650" height="650" alt={i}/>
                     </div>))
                 }
             </Slider>
-            <h3>{`${versionText} | ${currentPoem.date.format("M.D.YYYY")}`}</h3>
+            <Wave
+                    text={`${versionText} | ${currentStage.date.format("M.D.YYYY")}`}
+                    iterations="1"
+                    effect="fadeIn"
+                    paused={this.state.isSelecting}
+                    effectDuration={1}/>
             <input
-                onChange={e => this.slider.slickGoTo(Math.round(e.target.value))}
+                onChange={e => {
+                    this.setState({sliderValue:e.target.value})
+                    this.slider.slickGoTo(Math.round(e.target.value))
+                }}
+                onMouseDown={e => this.setState({isSelecting:true})}
+                onMouseUp={e => this.setState({isSelecting:false})}
                 class="form-range"
-                // value={this.state.currentStage}
+                value={this.state.sliderValue}
                 type="range"
                 step="0.01"
                 min="0"
-                max={poems.length-1}
+                max={stages.length-1}
             />
     
         </div>;
