5c955f89a012ccc9c9d20c3bfdf2cd75de52c6b4
wlongmire <warrenlongmire@gmail.com>
2022-11-19 14:33:33 -0500

    adding test for home page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import './../styles/tree.css'
+
 import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
--- pages/blackouts/index.js
+++ pages/blackouts/index.js
@@ -1,3 +1,4 @@
+
 import styled from 'styled-components';
 import { Plock } from 'react-plock';
 import Link from 'next/link';
@@ -14,7 +15,6 @@ const ListElement = styled.li`
   display:block;
 `
 
-
 const Home = () => {
   const breakpoints = [
     { size: 640, columns: 1 },
@@ -22,11 +22,66 @@ const Home = () => {
   ];
 
   return <>
-    <div>
-      <h1>Highlights & Blackouts</h1>
-      <p>A Poem Series By Heather Bowlan</p>
-    </div>
-    <Plock breakpoints={breakpoints}>
+    <ul className="tree">
+      <li>
+        <code id="header">
+          <h1>Highlights & Blackouts</h1>
+          <p>A Poem Series By Heather Bowlan</p>
+        </code>
+        <ul>
+          <li>
+            <code>The Series</code>
+            <ul>
+              <li>
+                <code><Link href="/series">The Writing Process</Link></code>
+              </li>
+              <li>
+                <code><Link href="/series#colaborations">Collaborations</Link></code>
+              </li>
+              <li>
+                <code><Link href="/series#tech_design">Technical Design</Link></code>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <code>The Artists</code>
+            <ul>
+              <li><code><Link href="/poet">Heather Bowlan</Link></code></li>
+              <li><code><Link href="/developer">Warren C. Longmire</Link></code></li>
+            </ul>
+          </li>
+          <li>
+            <code>The Contributors</code>
+            <ul>
+              {
+                contributors.map(contrib => <li><code><a href={`/contributors#${contrib.first}-${contrib.last}`}>{contrib.first} {contrib.last}</a></code></li>)
+              }
+            </ul>
+          </li>
+          <li>
+            <code>The Poems</code>
+            {
+              erasures.items.map((item) => {
+                return(
+                  <ul key={item.id} >
+                    <code><p><Link href={`/blackouts/${item.id}`}><a>{item.title}</a></Link></p></code>
+                    {/* <ul>
+                      {
+                        item.stages.map((stage) => 
+                          <code key={`${item.title}-${stage.title}`}>
+                            <Link href={`/blackouts/${item.id}/${stage.id}`}><a>{stage.title}</a></Link>
+                          </code>)
+                      }
+                    </ul> */}
+                </ul>)
+              })
+            }
+          </li>
+        </ul>
+      </li>
+      
+    </ul>
+    {/* <Plock breakpoints={breakpoints}>
       <Container>
         <h3>The Series</h3>
         <ul>
@@ -74,8 +129,9 @@ const Home = () => {
           })
         }
       </Container>
-    </Plock>
+    </Plock> */}
   </>
 }
 
 export default Home;
+
