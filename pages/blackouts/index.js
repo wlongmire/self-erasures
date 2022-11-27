c5dbad5b47ba061e9531998e93f468be4b48b2e2
wlongmire <warrenlongmire@gmail.com>
2022-11-27 18:36:14 -0500

    adds in seperate container bundles.

--- pages/blackouts/index.js
+++ pages/blackouts/index.js
@@ -1,6 +1,9 @@
 
 import styled from 'styled-components';
 import { Tree } from 'antd';
+import { Collapse } from 'antd';
+const { Panel } = Collapse;
+
 const { DirectoryTree } = Tree;
 
 import Link from 'next/link';
@@ -8,82 +11,127 @@ import Link from 'next/link';
 import erasures from './../data/erasures.json';
 import contributors from './../data/contributions';
 
-const treeData = [
-  {
-    title: 'The Series',
-    type: "header",
-    key: '0-0',
-    children:[
-      {
-        title: 'The Writing Process',
-        href:"/series",
-        type: "link",
-        key: '0-0-0'
-      },
-      {
-        title: 'Collaborations',
-        href:"/series#collaborations",
-        type: "link",
-        key: '0-0-1'
-      },
-      {
-        title: 'The Writing Process',
-        href:"/series#tech_design",
-        type: "link",
-        key: '0-0-2'
-      }
-    ]
-  },
-  {
-    title: 'The Artists',
-    type: "header",
-    key: '0-1',
-    children:[
-      {
-        title: 'Heather Bowlan',
-        href:"/poet",
-        type: "link",
-        key: '0-1-0'
-      },
-      {
-        title: 'Warren C. Longmire',
-        href:"/developer",
-        type: "link",
-        key: '0-1-1'
-      }
-    ]
-  },
-  {
-    title: 'The Contributors',
-    type: "header",
-    key: '0-2',
-    children: contributors.map((contrib, id) => ({
-      title: `${contrib.first} ${contrib.last}`,
-      href: `/contributors#${contrib.first}-${contrib.last}`,
+
+const treeData = {
+  series:[
+    {
+      title: 'The Writing Process',
+      href:"/series",
       type: "link",
-      key: `0-2-${id}`
-    }))
-  },
-  {
-    title: 'The Poems',
-    type: "header",
-    key: '0-3',
-    children: erasures.items.map(item => (
-      {
-        title: item.title,
-        type: "erasureTitle",
-        href:`/blackouts/${item.id}`,
-        key:`0-3-${item.id}`,
-        children: item.stages.map(stage => ({
-          title: stage.title,
-          type: "poemTitle",
-          key:`0-3-${item.id}-${stage.id}`,
-          href: `/blackouts/${item.id}/${stage.id}`
-        }))
-      }
-    ))
-  }
-];
+      key: '0-0-0'
+    },
+    {
+      title: 'Collaborations',
+      href:"/series#collaborations",
+      type: "link",
+      key: '0-0-1'
+    },
+    {
+      title: 'The Writing Process',
+      href:"/series#tech_design",
+      type: "link",
+      key: '0-0-2'
+    }
+  ],
+  artists:[
+    {
+      title: 'Heather Bowlan',
+      href:"/poet",
+      type: "link",
+      key: '0-1-0'
+    },
+    {
+      title: 'Warren C. Longmire',
+      href:"/developer",
+      type: "link",
+      key: '0-1-1'
+    }
+  ],
+  contributors:contributors.map((contrib, id) => ({
+    title: `${contrib.first} ${contrib.last}`,
+    href: `/contributors#${contrib.first}-${contrib.last}`,
+    type: "link",
+    key: `0-2-${id}`
+  })),
+  poems:[]
+}
+
+// const treeData = [
+//   {
+//     title: 'The Series',
+//     type: "header",
+//     key: '0-0',
+//     children:[
+//       {
+//         title: 'The Writing Process',
+//         href:"/series",
+//         type: "link",
+//         key: '0-0-0'
+//       },
+//       {
+//         title: 'Collaborations',
+//         href:"/series#collaborations",
+//         type: "link",
+//         key: '0-0-1'
+//       },
+//       {
+//         title: 'The Writing Process',
+//         href:"/series#tech_design",
+//         type: "link",
+//         key: '0-0-2'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'The Artists',
+//     type: "header",
+//     key: '0-1',
+//     children:[
+//       {
+//         title: 'Heather Bowlan',
+//         href:"/poet",
+//         type: "link",
+//         key: '0-1-0'
+//       },
+//       {
+//         title: 'Warren C. Longmire',
+//         href:"/developer",
+//         type: "link",
+//         key: '0-1-1'
+//       }
+//     ]
+//   },
+//   {
+//     title: 'The Contributors',
+//     type: "header",
+//     key: '0-2',
+//     children: contributors.map((contrib, id) => ({
+//       title: `${contrib.first} ${contrib.last}`,
+//       href: `/contributors#${contrib.first}-${contrib.last}`,
+//       type: "link",
+//       key: `0-2-${id}`
+//     }))
+//   },
+//   {
+//     title: 'The Poems',
+//     type: "header",
+//     key: '0-3',
+//     children: erasures.items.map(item => (
+//       {
+//         title: item.title,
+//         type: "erasureTitle",
+//         href:`/blackouts/${item.id}`,
+//         key:`0-3-${item.id}`,
+//         children: item.stages.map(stage => ({
+//           title: stage.title,
+//           type: "poemTitle",
+//           key:`0-3-${item.id}-${stage.id}`,
+//           href: `/blackouts/${item.id}/${stage.id}`
+//         }))
+//       }
+//     ))
+//   }
+// ];
 
 const TreeHeader = styled.h2`
   font-family: 'Sorts Mill Goudy', serif;
@@ -122,6 +170,12 @@ const B = styled.span`
   }
 `
 
+const TreeCardGroup = styled.div`
+  display:flex;
+`
+
+const TreeCard = styled.div`
+`
 
 const Header = styled.header`
   padding-bottom: 1em;
@@ -173,6 +227,44 @@ const TreeNode = (props) => {
 }
 
 const Home = () => {
+
+  const generateGroups = (numCards, content, bundleFn) => {
+    const groups = []
+    let g_idx = 0
+
+    content.forEach((c, idx)=> {
+      g_idx = Math.floor(idx / numCards)
+      if (!groups[g_idx])
+        groups.push([])
+  
+      groups[g_idx].push(bundleFn(c, g_idx, idx))
+    })
+
+    return groups
+  }
+
+  const contrib_groups = generateGroups(5, contributors, (c, g_idx, idx)=> ({
+      title: `${c.first} ${c.last}`,
+      href: `/contributors#${c.first}-${c.last}`,
+      type: "link",
+      key: `0-2-${g_idx}-${idx}`
+    })
+  )
+
+  const poem_groups = generateGroups(2, erasures.items, (c, g_idx, idx)=> ({
+      title: c.title,
+      type: "erasureTitle",
+      href:`/blackouts/${c.id}`,
+      key:`0-3-${c.id}`,
+      children: c.stages.map(stage => ({
+        title: stage.title,
+        type: "poemTitle",
+        key:`0-3-${c.id}-${stage.id}`,
+        href: `/blackouts/${c.id}/${stage.id}`
+      }))
+    })
+  )
+
   return <div>
     <Header>
       <span id="highlights">H<B>IG</B>HLIGHTS</span>
@@ -181,7 +273,62 @@ const Home = () => {
       <p>A Self-Erasure Series By <Link href="/poet"><B link>Heather Bowlan</B></Link> & <Link href="/developer"><B link>Warren C. Longmire</B></Link></p>
     </Header>
     <div>
-      <DirectoryTree
+      <Collapse ghost>
+        <Panel header="Series" key="1">
+          <DirectoryTree
+            showLine
+            autoExpandParent={true}
+            switcherIcon={<></>}
+            showIcon={false}
+            selectable={false}
+            treeData={treeData.series}
+          />
+        </Panel>
+        <Panel header="The Artists" key="2">
+          <DirectoryTree
+            showLine
+            autoExpandParent={true}
+            switcherIcon={<></>}
+            showIcon={false}
+            selectable={false}
+            treeData={treeData.artists}
+          />
+        </Panel>
+        <Panel header="The Contributors" key="3">
+          <TreeCardGroup>
+            {
+              contrib_groups.map(group => <div>
+                <DirectoryTree
+                  showLine
+                  autoExpandParent={true}
+                  switcherIcon={<></>}
+                  showIcon={false}
+                  selectable={false}
+                  treeData={group}
+                />      
+              </div>)
+            }
+          </TreeCardGroup>
+        </Panel>
+        <Panel header="The Poems" key="4">
+          <TreeCardGroup>
+            {
+                poem_groups.map(group => <div>
+                  <DirectoryTree
+                    showLine
+                    autoExpandParent={true}
+                    switcherIcon={<></>}
+                    showIcon={false}
+                    selectable={false}
+                    treeData={group}
+                  />      
+                </div>)
+              }
+            </TreeCardGroup>
+        </Panel>
+      </Collapse>
+
+      {/* <DirectoryTree
         showLine
         autoExpandParent={true}
         switcherIcon={<></>}
@@ -189,7 +336,7 @@ const Home = () => {
         selectable={false}
         treeData={treeData}
         titleRender={(data)=> <TreeNode {...data}/>}
-      />
+      /> */}
     </div>
   </div>
 }
