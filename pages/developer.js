536f3ce763445a1c10cc1a01a1de7b44f76e0a07
wlongmire <warrenlongmire@gmail.com>
2023-07-03 10:34:27 -0400

    swapped creater links.

--- pages/developer.js
+++ pages/developer.js
@@ -11,7 +11,7 @@ export default function developerPage() {
                 <p>As writer working at the intersection of arts and technology, Warren has developed two one-man shows, <em>Do.Until.True (2012)</em> and <em>It in the Time of Moore (2014)</em> in addition to stage proformance around his two most recent full length books <em>Open Source (2021 Radiator Press)</em> and <em>Open Source (2022 Bunny Presse)</em>. He is the founder of _mixlit, a event series and press dedicated to promoting and celibrating artists working at the intersection of art, music and technology.</p>
                 
                 <p>Warren is the host of House Poet: A Spoken Word Dance Party and a board member for the Philadelphia literary organization <a src="https://www.bluestoop.org/">Blue Stoop.</a> He has been published in journals including Cartridge Lit, The Cleveland Review of Books, The American Poetry Review. He was featured in the Best American Poetry 2021 selected by Tracey K. Smith. He lives in Philadelphia with his cat Poe.</p>
-                <h4 className="lh-lg">You can find more of Warren's work at <BlockErase><a href="https://www.heatherbowlan.com/">alongmirewriter.com</a></BlockErase></h4>
+                <h4 className="lh-lg">You can find more of Warren's work at <BlockErase><a href="https://www.alongmirewriter.com/">alongmirewriter.com</a></BlockErase></h4>
             </div>
         </div>
     </div>);
--- pages/poet.js
+++ pages/poet.js
@@ -8,7 +8,7 @@ export default function poetPage() {
             <div className="p-3 display-7">
                 <p>Heather Bowlan's work has appeared in the anthology Feminisms in Motion, the journals New Ohio Review, Nashville Review, the Anarchist Review of Books, the Ploughshares blog, and elsewhere. Heather has served as an editor for BOAAT Press and Raleigh Review and as the Operations Coordinator for the 215 Festival. She completed her MFA in Creative Writing at North Carolina State University and has received fellowships from the Virginia Center for the Creative Arts and the Vermont Studio Center.</p>
                 <p>In addition to her writing work, Heather is currently working in mixed-media collage, many of which are included in the print version of Highlights and Blackouts. She has recently begun showing my work in Galleries around Philadelphia. Her collages are inspired by the idea of precarious survival, a term coined by anthropologist Anna Lowenhaupt Tsing that challenges capitalist ideas of individual selves. People are enmeshed in our shared histories, and this awareness opens possibilities for community transformation. </p>
-                <h4 className="lh-lg">You can find more of Heather's work at <BlockErase><a href="https://www.alongmirewriter.com/">heatherbowlan.com</a></BlockErase></h4>
+                <h4 className="lh-lg">You can find more of Heather's work at <BlockErase><a href="https://www.heatherbowlan.com/">heatherbowlan.com</a></BlockErase></h4>
             </div>
         </div>
     </div>);
